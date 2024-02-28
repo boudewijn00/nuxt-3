6d48e1750ce9d11a89c6a1d13b058552b462846a
boudewijn overvliet <boudewijn@hello-data.nl>
2024-02-28 22:10:56 +0100

    wip

---
+++ pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+    <form @submit.prevent="login">
+        <input type="email" v-model="email" placeholder="email" />
+        <input type="password" v-model="password" placeholder="password" />
+        <button type="submit">Login</button>
+        <ul class="login-errors">
+            <li style="color: red" v-for="error in errors" :key="error">{{ error }}</li>
+        </ul>
+    </form>
+</template>
+<script setup>
+    definePageMeta({
+        middleware: ['guest']
+    });
+
+    import Cookies from 'js-cookie';
+    
+    const email = ref('');
+    const password = ref('');
+    const errors = ref([]);
+
+    const { $apiFetch } = useNuxtApp();
+    const { setUser } = useAuth();
+
+    async function login() {
+        await $apiFetch('/sanctum/csrf-cookie', {
+            method: 'get',
+            headers: {
+                'Accept': 'application/json',
+            }
+        });
+
+        try {
+            await $apiFetch('/login', {
+                method: 'post',
+                body: {
+                    email: email.value,
+                    password: password.value
+                },
+                headers: {
+                    'Accept': 'application/json',
+                    'X-XSRF-TOKEN': Cookies.get('XSRF-TOKEN'),
+                }
+            })
+
+            const response = await $apiFetch('/api/user', {
+                method: 'get',
+                headers: {
+                    'Accept': 'application/json',
+                    'X-XSRF-TOKEN': Cookies.get('XSRF-TOKEN'),
+            }});
+
+            setUser(response.user.name);
+            window.location.href = '/profile';
+        } catch (error) {
+            console.log('login error', error.data.errors);
+            errors.value = Object.values(error.data.errors).flat();
+        }
+
+    }
+
+</script>
