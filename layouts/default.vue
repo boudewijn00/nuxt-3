e4c2424d618a72ae37409ac9879dc1db8fbf6596
boudewijn overvliet <boudewijn@hello-data.nl>
2024-02-29 12:26:34 +0100

    wip

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,5 +12,6 @@
     </div>    
 </template>
 <script setup>
+    import 'primevue/resources/themes/aura-light-green/theme.css'
     const { isLoggedIn } = useAuth();
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,25 @@
+import path from 'path';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: [
     '~/assets/css/main.css',
-    'primevue/resources/themes/aura-light-green/theme.css'
   ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   modules: [
     'nuxt-primevue'
   ],
+  primevue: {
+    options: { unstyled: false },
+    cssLayerOrder: "tailwind-base, primevue, tailwind-utilities",
+    importPT: { from: path.resolve(__dirname, './presets/lara/') }      //import and apply preset   
+  },
   runtimeConfig: {
     public: {
       apiBaseUrl: process.env.API_BASE_URL
