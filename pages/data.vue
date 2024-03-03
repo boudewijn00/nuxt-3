61187c1835f52f449c992b309256a85a1b826d6e
Boudewijn Overvliet <boudewijnovervliet@Boudewijns-Mac-Studio.fritz.box>
2024-03-03 21:33:06 +0100

    wip

--- pages/data.vue
+++ pages/data.vue
@@ -14,15 +14,15 @@
                                     <span class="font-medium text-secondary text-sm">{{ item.category }}</span>
                                     <div class="text-lg font-medium text-900 mt-2">{{ item.name }}</div>
                                 </div>
-                                <div class="surface-100 p-1" style="border-radius: 30px">
+                                <div class="surface-100" style="border-radius: 30px">
                                     <div class="surface-0 flex align-items-center gap-2 justify-content-center py-1 px-2" style="border-radius: 30px; box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.04), 0px 1px 2px 0px rgba(0, 0, 0, 0.06)">
                                         <span class="text-900 font-medium text-sm">{{ item.rating }}</span>
                                         <i class="pi pi-star-fill text-yellow-500"></i>
                                     </div>
                                 </div>
                             </div>
-                            <div class="flex flex-column md:align-items-end gap-5">
-                                <span class="text-xl font-semibold text-900 my-1">${{ item.price }}</span>
+                            <div class="flex flex-column md:align-items-end">
+                                <span class="text-xl font-semibold text-900">${{ item.price }}</span>
                             </div>
                         </div>
                     </div>
