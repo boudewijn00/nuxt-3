60f41fffd3240eac2f9f1cf639905dc0274c1bb5
Boudewijn Overvliet <boudewijnovervliet@Boudewijns-Mac-Studio.fritz.box>
2024-03-03 21:52:11 +0100

    wip

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="my-5">
         <p class="text-xl mb-5">Profile</p>
+        <MeterGroup :value="value" />
+    </div>
+    <div class="my-5">
         <Fieldset legend="Header">
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
         </Fieldset>
-    </div>
-    <div class="my-5">
-        <MeterGroup :value="value" />
     </div>
 </template>
 <style scoped>
@@ -25,5 +25,5 @@ import { RouterLink } from 'vue-router';
     });
 
     const router = useRoute();
-    const value = ref([{ label: 'Space used', value: 15 }]);
+    const value = ref([{ label: 'Profile Completed', value: 15 }]);
 </script>
