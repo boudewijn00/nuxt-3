ba140925a3da505970439f209a851c148a73167d
Boudewijn Overvliet <boudewijnovervliet@Boudewijns-Mac-Studio.fritz.box>
2024-03-03 21:40:59 +0100

    wip

--- pages/steps.vue
+++ pages/steps.vue
@@ -17,7 +17,7 @@
                     <div class="flex flex-column h-12rem">
                         <div class="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium p-5">Content II</div>
                     </div>
-                    <div class="flex pt-4 justify-content-between">
+                    <div class="flex pt-4 justify-content-between gap-2">
                         <Button label="Back" severity="secondary" icon="pi pi-arrow-left" @click="prevCallback" />
                         <Button label="Next" icon="pi pi-arrow-right" iconPos="right" @click="nextCallback" />
                     </div>
