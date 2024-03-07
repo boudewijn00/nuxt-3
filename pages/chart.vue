d8ee489e97cf3679cac701bfd99860beeaffa5b2
Boudewijn Overvliet <boudewijnovervliet@BoudewiacStudio.fritz.box>
2024-03-07 09:15:46 +0100

    wip

---
+++ pages/chart.vue
@@ -0,0 +1,7 @@
+<template>
+<div class="h-screen">
+<iframe src="https://good-data-vuejs.hello-data.nl/?insight_id=4b92cb0d-f68d-4f1a-bfb2-6a5835ec105c" class="w-full h-2/5 overflow-hidden">
+
+</iframe>
+</div>
+</template>
